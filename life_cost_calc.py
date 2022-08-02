f08c5f2b88ef47d2e25f2f0481d66e1ba79b8d55
YuyaIsh <101812706+YuyaIsh@users.noreply.github.com>
2022-08-02 15:52:20 +0900

    Add files via upload

--- life_cost_calc.py
+++ life_cost_calc.py
@@ -52,7 +52,7 @@ def main():
         df_concat = pd.concat([df_current_month, df_pre_month])
         col_df,col_delete = st.columns(2)
         with col_df:
-            st.dataframe(df_concat.iloc[::-1],height=300)
+            st.dataframe(df_concat.iloc[::-1],height=250)
 
         with col_delete:
             try:
